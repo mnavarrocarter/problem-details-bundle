4ac9169a0a8d311b600237721728a4f811de6a8d
Matías Navarro Carter <mnavarro@option.cl>
2018-04-19 10:24:06 -0300

    Test suite mounted

---
+++ Tests/DependencyInjection/MNCProblemDetailsExtensionTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MNC\Bundle\ProblemDetailsBundle\Tests\DependencyInjection;
+
+use MNC\Bundle\ProblemDetailsBundle\DependencyInjection\MNCProblemDetailsExtension;
+use PHPUnit\Framework\TestCase;
+
+class MNCProblemDetailsExtensionTest extends TestCase
+{
+    public function testLoad()
+    {
+        $this->assertSame(null, null);
+    }
+}
